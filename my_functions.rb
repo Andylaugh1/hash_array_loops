58e52d1b3523d4c81acc557f7d8c12747c84b6ca
Andy Laughlin <atl11384@gmail.com>
2018-04-11 15:43:12 +0100

    add numbers test successful

---
+++ my_functions.rb
@@ -0,0 +1,15 @@
+def add_array_lengths(array1, array2)
+  add_array1 = array1.length
+  add_array2 = array2.length
+  return add_array1 + add_array2
+end
+
+counter = 0
+def sum_array(numbers)
+  for number in numbers
+    counter += number
+  end
+end
+
+return counter
+  
